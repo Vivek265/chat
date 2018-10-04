de2bac94a2c1a4ffdbd0d4332c2569ffe0967fd6
Vivekgopinath265 <vivekgopinath265@gmail.com>
2018-10-04 20:53:51 +0530

    changed

--- index.js
+++ index.js
@@ -14,12 +14,47 @@ app.get('/',(req,res)=>{
     res.sendFile(__dirname +'./public/index.html');
 
 });
+var currentUsers=[];
+var offline=[];
+var userName;
 io.on('connection',(socket)=>{
-console.log('connected');
+console.log('connected',socket.id);
+socket.on('login',(username)=>{
+var user={
+    name:username,
+    id:socket.id
+};userName=user.name;
+for(x in offline){
+    if(offline[x].to==userName){
+    io.to(user.id).emit('chat message',offline[x]);
+    delete offline[x];}
+}
+
+console.log(user);
+currentUsers.push(user);
+console.log(currentUsers);
+});
+
 socket.on('disconnect',()=>{
-    console.log('user disconnected')
+    console.log('user disconnected');
+    for(x in currentUsers)
+    if(currentUsers[x].name==userName)
+     delete currentUsers[x];
 });
 socket.on('chat',(msg)=>{
-    socket.broadcast.emit('chat message',msg);
+    msg.from=userName;
+    for (x in currentUsers){
+        if(currentUsers[x].name==msg.to)
+    io.to(currentUsers[x].id).emit('chat message',msg);
+else if(currentUsers[x].name!=msg.to && x==(currentUsers.length-1) )
+         {
+             var offlineUser={
+                 from:userName,
+                 to:msg.to,
+                 txt:msg.txt
+             };console.log(offlineUser);
+             offline.push(offlineUser);
+             console.log(offline);
+         }}
 });
 });
